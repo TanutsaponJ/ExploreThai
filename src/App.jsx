b47bd262388af5dc56fef7ee9e263db5963609ab
TanutsaponJ <anakin112333@gmail.com>
2024-02-13 12:16:13 -0500

    Refactor import statements in App.jsx and add index.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,30 @@ import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./components/home/Home";
-import Articles1 from "./pages/Inspiring/articles/Articles1";
-import Articles2 from "./pages/Inspiring/articles/Articles2";
-import Articles3 from "./pages/Inspiring/articles/Articles3";
-import Articles4 from "./pages/Inspiring/articles/Articles4";
-import Articles5 from "./pages/Inspiring/articles/Articles5";
-import DestinationArticles1 from "./pages/destination/DestinationArticles/DestinationArticles1";
-import DestinationArticles2 from "./pages/destination/DestinationArticles/DestinationArticles2";
-import DestinationArticles3 from "./pages/destination/DestinationArticles/DestinationArticles3";
-import DestinationArticles4 from "./pages/destination/DestinationArticles/DestinationArticles4";
-import DestinationArticles5 from "./pages/destination/DestinationArticles/DestinationArticles5";
-import DestinationArticles6 from "./pages/destination/DestinationArticles/DestinationArticles6";
-import SeasonFebruary from "./pages/season/seasonList/SeasonFebruary";
-import SeasonMarch from "./pages/season/seasonList/SeasonMarch";
-import SeasonApril from "./pages/season/seasonList/SeasonApril";
-import SeasonMay from "./pages/season/seasonList/SeasonMay";
-import SeasonJune from "./pages/season/seasonList/SeasonJune";
-import SeasonJuly from "./pages/season/seasonList/SeasonJuly";
-import SeasonAugust from "./pages/season/seasonList/SeasonAugust";
-import SeasonSeptember from "./pages/season/seasonList/SeasonSeptember";
-import SeasonOctober from "./pages/season/seasonList/SeasonOctober";
-import SeasonNovember from "./pages/season/seasonList/SeasonNovember";
-import SeasonDecember from "./pages/season/seasonList/SeasonDecember";
-import SeasonJanuary from "./pages/season/seasonList/SeasonJanuary";
+import { Articles1, Articles2, Articles3, Articles4, Articles5 } from "./index";
+import {
+  DestinationArticles1,
+  DestinationArticles2,
+  DestinationArticles3,
+  DestinationArticles4,
+  DestinationArticles5,
+  DestinationArticles6,
+} from "./index";
+
+import {
+  SeasonFebruary,
+  SeasonMarch,
+  SeasonApril,
+  SeasonMay,
+  SeasonJune,
+  SeasonJuly,
+  SeasonAugust,
+  SeasonSeptember,
+  SeasonOctober,
+  SeasonNovember,
+  SeasonDecember,
+  SeasonJanuary,
+} from "./index";
 
 const Layout = () => {
   return (
---
+++ src/index.js
@@ -0,0 +1,25 @@
+export { default as Articles1 } from "./pages/Inspiring/articles/Articles1";
+export { default as Articles2 } from "./pages/Inspiring/articles/Articles2";
+export { default as Articles3 } from "./pages/Inspiring/articles/Articles3";
+export { default as Articles4 } from "./pages/Inspiring/articles/Articles4";
+export { default as Articles5 } from "./pages/Inspiring/articles/Articles5";
+
+export { default as DestinationArticles1 } from "./pages/destination/DestinationArticles/DestinationArticles1";
+export { default as DestinationArticles2 } from "./pages/destination/DestinationArticles/DestinationArticles2";
+export { default as DestinationArticles3 } from "./pages/destination/DestinationArticles/DestinationArticles3";
+export { default as DestinationArticles4 } from "./pages/destination/DestinationArticles/DestinationArticles4";
+export { default as DestinationArticles5 } from "./pages/destination/DestinationArticles/DestinationArticles5";
+export { default as DestinationArticles6 } from "./pages/destination/DestinationArticles/DestinationArticles6";
+
+export { default as SeasonFebruary } from "./pages/season/seasonList/SeasonFebruary";
+export { default as SeasonMarch } from "./pages/season/seasonList/SeasonMarch";
+export { default as SeasonApril } from "./pages/season/seasonList/SeasonApril";
+export { default as SeasonMay } from "./pages/season/seasonList/SeasonMay";
+export { default as SeasonJune } from "./pages/season/seasonList/SeasonJune";
+export { default as SeasonJuly } from "./pages/season/seasonList/SeasonJuly";
+export { default as SeasonAugust } from "./pages/season/seasonList/SeasonAugust";
+export { default as SeasonSeptember } from "./pages/season/seasonList/SeasonSeptember";
+export { default as SeasonOctober } from "./pages/season/seasonList/SeasonOctober";
+export { default as SeasonNovember } from "./pages/season/seasonList/SeasonNovember";
+export { default as SeasonDecember } from "./pages/season/seasonList/SeasonDecember";
+export { default as SeasonJanuary } from "./pages/season/seasonList/SeasonJanuary";
